c3e0dab2b5fcf00b9a1207e58f69f19073d7da67
Kisalay <kisalaykisu@gmail.com>
2022-07-22 23:28:05 +0530

    UC5 has been added

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class CabInvoiceGeneratorTest {
 
     CabInvoiceGenerator fare;
@@ -18,23 +19,23 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
-        Assertions.assertEquals(70, fare.calculateFare(5, 20), 0.000);
+        Assertions.assertEquals(70, fare.calculateFare(5, 20, CabInvoiceGenerator.RideType.NORMAL_RIDE), 0.000);
     }
 
     @Test
     public void whenGivenMultipleFaresShouldReturnAggregateTotalFare() {
-        Ride[] ridesArray = {new Ride("1",2, 20),
-                new Ride("1",4, 10),
-                new Ride("2",10, 30)};
+        Ride[] ridesArray = {new Ride("1",2, 20, CabInvoiceGenerator.RideType.NORMAL_RIDE),
+                new Ride("1",4, 10, CabInvoiceGenerator.RideType.NORMAL_RIDE),
+                new Ride("2",10, 30, CabInvoiceGenerator.RideType.NORMAL_RIDE)};
         Assertions.assertEquals(220, fare.calculateFareForMultipleRides(ridesArray), 0.0);
     }
 
     @Test
     public void givenDistanceAndTimeWhenAddedShouldReturnInvoiceSummary() {
         Ride[] rides = {
-                new Ride("1",2, 5),
-                new Ride("2",3, 5),
-                new Ride("1",1, 1)
+                new Ride("1",2, 5, CabInvoiceGenerator.RideType.NORMAL_RIDE),
+                new Ride("2",3, 5, CabInvoiceGenerator.RideType.NORMAL_RIDE),
+                new Ride("1",1, 1, CabInvoiceGenerator.RideType.NORMAL_RIDE)
         };
         InvoiceSummary invoiceSummary = fare.invoiceSummaryCalculation(rides);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3, 71);
@@ -44,12 +45,12 @@ public class CabInvoiceGeneratorTest {
     @Test
     public void givenUserIdDistanceAndTimeShouldReturnInvoiceOfUser() {
         Ride[] rides = {
-                new Ride("1",2, 5),
-                new Ride("2",3, 5),
-                new Ride("1",1, 1)
+                new Ride("1",2, 5, CabInvoiceGenerator.RideType.PREMIUM_RIDE),
+                new Ride("2",3, 5, CabInvoiceGenerator.RideType.PREMIUM_RIDE),
+                new Ride("2",1, 1, CabInvoiceGenerator.RideType.PREMIUM_RIDE)
         };
-        InvoiceSummary invoiceSummary = fare.listOfRidesOfUniqueUserId(rides,"1");
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 36);
+        InvoiceSummary invoiceSummary = fare.listOfRidesOfUniqueUserId(rides,"2");
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 75);
         Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
 }
